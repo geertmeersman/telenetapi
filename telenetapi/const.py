599359efe0e6694227a33a602287ca128221d698
Geert Meersman <geertmeersman+github@gmail.com>
2024-02-05 16:49:00 +0100

    fix: psf/black

--- telenetapi/const.py
+++ telenetapi/const.py
@@ -1,4 +1,5 @@
 """The constants for telenetapi."""
+
 from .models import TelenetEnvironment
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
